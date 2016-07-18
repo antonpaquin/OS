840d4e6ad59a3f5744787ac03923ea00bd407ab0
Anton <antonpaquin@gmail.com>
2016-07-18 14:09:45 -0700

    Install xorg

--- script3.sh
+++ script3.sh
@@ -10,3 +10,5 @@ systemctl start docker &&
 
 mkdir /usr/dockerfiles &&
 mkdir /usr/dockerfiles/xorg &&
+curl https://raw.githubusercontent.com/antonpaquin/OS/master/xorg.docker > /usr/dockerfiles/xorg/Dockerfile &&
+docker build -t xorg /usr/dockerfiles/xorg &&
