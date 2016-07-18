64367e7b8b49d3f8545701a4a9f2c5e6194a2e00
Anton <antonpaquin@gmail.com>
2016-07-17 18:52:56 -0700

    Remove chroot from script1, add && to script2

--- script2.sh
+++ script2.sh
@@ -1,16 +1,16 @@
-ln -s /usr/share/zoneinfo/US/Pacific /etc/localtime
+ln -s /usr/share/zoneinfo/US/Pacific /etc/localtime &&
 
-cat /etc/locale.gen | grep en_US | tail -n 2 | cut -c 2- > /etc/locale.gen.tmp
-mv /etc/locale.gen.tmp /etc/locale.gen
-locale-gen
+cat /etc/locale.gen | grep en_US | tail -n 2 | cut -c 2- > /etc/locale.gen.tmp &&
+mv /etc/locale.gen.tmp /etc/locale.gen &&
+locale-gen &&
 
-echo "Nimitz" > /etc/hostname
+echo "Nimitz" > /etc/hostname &&
 
-curl "http://antonpaquin.me/arch/ps50" > /etc/netctl/ps50
+curl "https://raw.githubusercontent.com/antonpaquin/OS/master/ps50" > /etc/netctl/ps50 &&
 
-mkinitcpio -p linux
+mkinitcpio -p linux &&
 
-passwd
+passwd &&
 
-pacman -S --noconfirm refind-efi
+pacman -S --noconfirm refind-efi &&
 echo "Exit chroot and reboot, the system should be installed!"
