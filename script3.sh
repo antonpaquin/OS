e27f44d30a76d19e8a7d77b8e3f9b66685305b56
Anton <antonpaquin@gmail.com>
2016-07-21 23:25:39 -0700

    I added more things to host =(

--- script3.sh
+++ script3.sh
@@ -1,4 +1,5 @@
-useradd -m anton &&
+groupadd docker &&
+useradd -g docker -m anton &&
 #Will go away
 echo "root:grub" | chpasswd &&
 echo "anton:anton" | chpasswd &&
@@ -13,18 +14,13 @@ mkdir /usr/dockerfiles &&
 #mkdir /usr/dockerfiles/xorg &&
 #curl https://raw.githubusercontent.com/antonpaquin/OS/master/xorg.docker > /usr/dockerfiles/xorg/Dockerfile &&
 #docker build -t xorg /usr/dockerfiles/xorg &&
-#
-#docker run \
-#  -v /tmp/.X11-unix:/tmp/.X11-unix \
-#  --device /dev:/dev \
-#  -it \
-#  -u user \
-#  xorg
 
-pacman -S --noconfirm xorg-server xorg-xinit xorg-xinput &&
-pacman -S --noconfirm xf86-input-libinput xf86-video-intel
+pacman -S --noconfirm xorg-server xorg-xinit xorg-xinput xorg-xhost &&
+pacman -S --noconfirm xf86-input-libinput xf86-video-intel &&
 
 pacman -S --noconfirm lightdm lightdm-gtk-greeter &&
 echo "greeter-session=lightdm-gtk-greeter" >> /etc/lightdm/lightdm.conf &&
 
-pacman -S --noconfirm fluxbox
+pacman -S --noconfirm fluxbox &&
+
+pacman -S --noconfirm git
