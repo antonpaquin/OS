fd7fb29e04877a8e4fb68917b6ba82e31f2fe79d
Anton Paquin <antonpaquin@gmail.com>
2016-07-23 17:54:57 -0700

    mkdir don't fail on already-exists

--- 04-docker.sh
+++ 04-docker.sh
@@ -1,6 +1,6 @@
 install() {
   cp /root/OS/RunScripts/$1 /usr/dockerscripts/$1 &&
-  mkdir /usr/dockerfiles/$1 &&
+  mkdir -p /usr/dockerfiles/$1 &&
   cp /root/OS/Dockerfiles/$1 /usr/dockerfiles/$1/Dockerfile &&
   chmod +x /usr/dockerscripts/$1 &&
   docker build -t $1 /usr/dockerfiles/$1
