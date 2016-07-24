d68585eccd5301b56f4f1ee13bbb566460340da2
Anton Paquin <antonpaquin@gmail.com>
2016-07-23 17:48:42 -0700

    docker install file initial

---
+++ 04-docker.sh
@@ -0,0 +1,10 @@
+install() {
+  cp /root/OS/RunScripts/$1 /usr/dockerscripts/$1 &&
+  mkdir /usr/dockerfiles/$1 &&
+  cp /root/OS/Dockerfiles/$1 /usr/dockerfiles/$1/Dockerfile &&
+  chmod +x /usr/dockerscripts/$1 &&
+  docker build -t $1 /usr/dockerfiles/$1
+}
+
+install atom
+install chromium
